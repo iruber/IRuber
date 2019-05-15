29fd5af5cafa3c621615dc6e143e4e3da7dd5d84
Gagurau <43859603+Gagurau@users.noreply.github.com>
2019-05-14 23:13:49 -0300

    Criação de telas de cadastro

---
+++ app/src/main/java/com/comercial/iruber/gui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.comercial.iruber.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.comercial.iruber.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/comercial/iruber/restaurante/gui/CadastroRestauranteActivity.java
@@ -0,0 +1,15 @@
+package com.comercial.iruber.restaurante.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.comercial.iruber.R;
+
+public class CadastroRestauranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_restaurante);
+    }
+}

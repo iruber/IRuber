fd6af825f23dc2a899f73e4f75fe713c54f56fbc
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-05-25 23:25:17 -0300

    update main login and main cadastro

--- app/src/main/java/com/comercial/iruber/Teste.java
+++
@@ -1,7 +0,0 @@
-package com.comercial.iruber;
-
-public class Teste{
-    public static void main(String[] args){
-        System.out.println("Hello World! ");
-    }
-}
--- app/src/main/java/com/comercial/iruber/usuario/gui/MainLogin.java
+++ app/src/main/java/com/comercial/iruber/usuario/gui/MainLogin.java
@@ -1,11 +1,10 @@
 package com.comercial.iruber.usuario.gui;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.comercial.iruber.R;
 
@@ -14,17 +13,22 @@ public class MainLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main_login_v);
+        Button btnloginrestaurante;
+        btnloginrestaurante =  findViewById(R.id.restauranteButton);
+        Button btnlogincliente;
+        btnlogincliente =  findViewById(R.id.clienteButton);
+        Button btnloginentregador;
+        btnloginentregador =  findViewById(R.id.entregadorButton);
+        Button btnCadastrar;
+        btnCadastrar = findViewById(R.id.cadastrarButton);
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent menuIntent;
+                menuIntent = new Intent(getApplicationContext(), MainCadastro.class);
+                startActivity(menuIntent);
             }
         });
     }
-
 }

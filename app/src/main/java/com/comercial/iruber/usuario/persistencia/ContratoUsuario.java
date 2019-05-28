e4d856a117186da96fd828c9c7abaaa878a6768c
Marcos Vinicius Peixoto <marcospeixotogk@gmail.com>
2019-05-28 19:11:09 -0300

    Refactor no contrato do banco de dados

---
+++ app/src/main/java/com/comercial/iruber/usuario/persistencia/ContratoUsuario.java
@@ -0,0 +1,17 @@
+package com.comercial.iruber.usuario.persistencia;
+
+public class ContratoUsuario {
+    public static final String NOME_TABELA = "usuarios";
+    public static final String USUARIO_ID = "usuarioId";
+    public static final String USUARIO_EMAIL = "email";
+    public static final String USUARIO_SENHA = "senha";
+    public static final String USUARIO_TIPO = "tipo";
+
+    public static final String SQL_CREATE_TABLE_USER = "CREATE TABLE "+NOME_TABELA+"(" +
+            USUARIO_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            USUARIO_SENHA+" TEXT NOT NULL, " +
+            USUARIO_EMAIL+" TEXT NOT NULL," +
+            USUARIO_TIPO+" TEXT ); ";
+
+    public static final String SQL_DELETE_USERS = "DROP TABLE IF EXISTS " + ContratoUsuario.NOME_TABELA;
+}

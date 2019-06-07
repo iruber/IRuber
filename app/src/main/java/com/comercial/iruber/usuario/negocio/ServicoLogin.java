f24d876e092516f659cd35ec90809bd7a103ed24
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-06-06 23:45:29 -0300

    sessao entregador

--- app/src/main/java/com/comercial/iruber/usuario/negocio/ServicoLogin.java
+++ app/src/main/java/com/comercial/iruber/usuario/negocio/ServicoLogin.java
@@ -7,6 +7,7 @@ import com.comercial.iruber.cliente.dominio.Cliente;
 import com.comercial.iruber.infra.EnumTipo;
 import com.comercial.iruber.infra.IruberException;
 import com.comercial.iruber.infra.Sessao;
+import com.comercial.iruber.restaurante.dominio.Entregador;
 import com.comercial.iruber.restaurante.dominio.Restaurante;
 import com.comercial.iruber.restaurante.persistencia.RestauranteDAO;
 import com.comercial.iruber.usuario.persistencia.UsuarioDAO;
@@ -40,6 +41,10 @@ public class ServicoLogin {
             Cliente cliente = clienteDAO.getClienteByIdUsuario(usuarioLogado.getId());
             Sessao sessao = new Sessao();
             sessao.editSessaoCliente(cliente, contexto);
+        }else if(usuarioLogado.getTipo() == EnumTipo.ENTREGADOR){
+            Entregador entregador = new Entregador();
+            Sessao sessao = new Sessao();
+            sessao.editSessaoEntregador(entregador, contexto);
         }
 
     }
